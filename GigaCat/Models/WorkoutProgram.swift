686334d80f3559590061d8f8daae9fcc9a370f61
amanbayserkeev0377 <amanbayserkeev0377@gmail.com>
2026-04-10 20:32:48 +0600

    test

--- GigaCat/Models/WorkoutProgram.swift
+++ GigaCat/Models/WorkoutProgram.swift
@@ -1,17 +1,1 @@
-import Foundation
-import SwiftData
 
-@Model
-final class WorkoutProgram {
-    var name: String
-    var icon: String        // SF Symbol name
-    var colorHex: String    // accent color
-    var createdAt: Date
-    
-    init(name: String, icon: String = "figure.strengthtraining.traditional", colorHex: String = "#FF6B35") {
-        self.name = name
-        self.icon = icon
-        self.colorHex = colorHex
-        self.createdAt = Date()
-    }
-}
